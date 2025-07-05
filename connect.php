4b1ab889d7e92ea6b3fcdeb330c77e75426c8434
mella <oyegbemilelatifat@gmail.com>
2025-07-05 09:52:07 +0100

    first commit

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    $conn = mysqli_connect("localhost", "aladmoso_jemray","Aladmoproject123$", "aladmoso_jemary");
+
+    if (!$conn) {
+        die("Connection failed : " . !$conn);
+    }else{
+
+    }
+
+?>
